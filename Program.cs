72680aa937dd206cd463b166bb0e2c1bb4ccd5f8
emmyobota1302 <obotaemelda@gmail.com>
2022-06-20 19:27:54 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string inputStr="";
+Console .WriteLine("Do while()loop");
+do{
+    inputStr=Console.ReadLine();
+    Console .WriteLine("You entered:{0}",inputStr);
+}
+while(inputStr!="exit") ;
+
+
+ 
